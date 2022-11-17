bcb80875c17c8c9c42a0fc7816cb170652a75e47
Nicola Placuzzi <NICOLA.PLACUZZI@local.ittcesena.it>
2022-11-17 09:18:32 +0100

    Versione Biblioteca con Proprieta

---
+++ Biblioteca_VerificaGit/Biblioteca.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca_VerificaGit
+{
+    internal class Biblioteca
+    {
+        internal List<Libro> Libri = new List<Libro>();
+        DateTime OrarioApertura { get; set; }
+        DateTime OrarioChiusura { get; set; }
+        string NomeBiblioteca { get; set; }
+        string IndirizzoBiblioteca { get; set; }
+
+
+        
+
+    }
+}
